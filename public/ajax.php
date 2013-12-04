cc2f9da2654166e7475876bf0fa420337c2f2c07
Hamish Hutchings <hamhut1066@gmail.com>
2013-12-04 19:07:48 +0000

    starting on ajax

---
+++ public/ajax.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<script src="https://code.jquery.com/jquery.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="/js/bootstrap.min.js"></script>
+<script>
+function showHint(str)
+{
+    if (str.length==0)
+          { 
+                document.getElementById("txtHint").innerHTML="";
+                  return;
+                  }
+    if (window.XMLHttpRequest)
+          {// code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp=new XMLHttpRequest();
+                  }
+    else
+          {// code for IE6, IE5
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+                  }
+    xmlhttp.onreadystatechange=function()
+          {
+                if (xmlhttp.readyState==4 && xmlhttp.status==200)
+                        {
+                                document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+                                    }
+                  }
+    xmlhttp.open("GET","gethint.php?q="+str,true);
+    xmlhttp.send();
+}
+</script>
+</head>
+<body>
+
+<p><b>Start typing a name in the input field below:</b></p>
+<form> 
+First name: <input type="text" onkeyup="showHint(this.value)">
+</form>
+<p>Suggestions: <span id="txtHint"></span></p>
+
+</body>
+</html>
